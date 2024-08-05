668ff1d520128229b7e2875378192376e1def7a9
miyago9267 <miyagomizugen@gmail.com>
2024-08-05 14:09:46 +0800

    feat: add a cross buttom to undo search text

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,12 @@
 <template>
-<div class="flex w-full justify-center">
+<div class="flex w-full justify-center relative">
     <input type="text"
     class="flex w-full h-8
-    rounded-md border-solid bg-tggray-300 border-1 px-4 bg-tggray-50 text-white"
+    rounded-md border-solid bg-tggray-300 border-1 px-4 bg-tggray-50 text-white pr-8"
     placeholder="搜尋表情包" v-model="search" @input="handleInput">
+    <div class="py-0 absolute right-3 h-full flex justify-center items-center">
+      <button v-if="search" @click="clearSearch" class="p-0 text-tggray-500">✕</button>
+    </div>
 </div>
 </template>
 
@@ -18,4 +21,17 @@ const handleInput = () => {
   emit('update:search', search.value);
 }
 
+const clearSearch = () => {
+  search.value = '';
+  emit('update:search', search.value);
+}
+
 </script>
+
+<style scoped>
+button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
