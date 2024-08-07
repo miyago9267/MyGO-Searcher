000e27d44c51cb81a5cf9387e86636b7ea7f759a
miyago9267 <miyagomizugen@gmail.com>
2024-08-07 16:52:29 +0800

    fix: copy to clipboard error when source change into jpg

--- src/components/ImageView.vue
+++ src/components/ImageView.vue
@@ -44,10 +44,29 @@ const copyToClipboard = async () => {
     const response = await fetch(props.url + '?t=' + new Date().getTime(), {
       method: 'GET',
     });
+    const convertJpegToPng = async (jpegBlob: Blob) => {
+      const img = document.createElement('img');
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+      return new Promise((resolve, reject) => {
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0);
+          canvas.toBlob((blob) => {
+            resolve(blob);
+          }, 'image/png');
+        };
+        img.onerror = reject;
+        img.src = URL.createObjectURL(jpegBlob);
+      });
+    };
     const blob = await response.blob();
+    const pngBlob = await convertJpegToPng(blob);
     const reader = new FileReader();
     reader.onloadend = async () => {
-      const clipboardItem = new ClipboardItem({ 'image/png': blob });
+      const clipboardItem = new ClipboardItem({ 'image/png': pngBlob as Blob });
       await navigator.clipboard.write([clipboardItem]);
     };
     reader.readAsDataURL(blob);
