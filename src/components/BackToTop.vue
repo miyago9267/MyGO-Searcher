916c6419abc1ab568c18e54b650d066fe187c6af
miyago9267 <miyagomizugen@gmail.com>
2024-08-09 10:45:12 +0800

    style: refect button style

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,6 +1,6 @@
 <template>
-    <button @click="backToTop" class="h-16 w-16 rounded-full border-gray-8 bg-gray-6 flex justify-center items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="36" height="36">
+    <button @click="backToTop" class="back-to-top flex justify-center items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24" height="24">
             <path d="M12 2l-10 10h6v10h8v-10h6z"/>
         </svg>
     </button>
@@ -13,3 +13,25 @@ const backToTop = () => {
 }
 
 </script>
+
+<style scoped>
+.back-to-top {
+    padding: 0.75rem;
+    cursor: pointer;
+    opacity: 0.7;
+    transition: opacity 0.3s;
+    background-color: rgb(75 85 99);
+    border-radius: 9999px;
+    border-width: 1px;
+    border-color: rgb(31 41 55) !important;
+}
+.back-to-top:hover {
+  opacity: 1;
+}
+button:focus,
+button:focus-visible {
+  outline: none;
+}
+
+
+</style>
