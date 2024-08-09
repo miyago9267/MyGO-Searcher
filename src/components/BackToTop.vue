e7caff645f1190e7bbe2ad4c38d60da63558a640
miyago9267 <miyagomizugen@gmail.com>
2024-08-09 10:25:04 +0800

    fix: button svg missing

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,6 +1,6 @@
 <template>
     <button @click="backToTop" class="h-16 w-16 rounded-full border-gray-8 bg-gray-6 flex justify-center items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="36" height="36">
             <path d="M12 2l-10 10h6v10h8v-10h6z"/>
         </svg>
     </button>
