6b64880f364eb299d9e9fef836dae0dd1808810f
miyago9267 <miyagomizugen@gmail.com>
2025-04-23 11:34:21 +0800

    chore: 移動檔案位置

--- server/utils/dataLoader.ts
+++ server/utils/dataLoader.ts
@@ -2,11 +2,11 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export const jsonData = (() => {
-  const dataPath = join(process.cwd(), 'data', 'image_map.json');
+  const dataPath = join(process.cwd(), 'public', 'data', 'image_map.json');
   return JSON.parse(readFileSync(dataPath, 'utf-8'));
 })();
 
 export const customKeyMap = (() => {
-  const dataPath = join(process.cwd(), 'data', 'custom_keymap.json');
+  const dataPath = join(process.cwd(), 'public', 'data', 'custom_keymap.json');
   return JSON.parse(readFileSync(dataPath, 'utf-8'));
 })();
