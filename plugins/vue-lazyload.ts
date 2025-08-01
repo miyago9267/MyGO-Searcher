6944d16d7f212671eb61e7b0158ed4db01afc234
miyago9267 <miyagomizugen@gmail.com>
2025-08-02 06:04:57 +0800

    chore: fix the image mapping

--- plugins/vue-lazyload.ts
+++ plugins/vue-lazyload.ts
@@ -3,7 +3,7 @@ import VueLazyload from 'vue-lazyload'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueLazyload, {
     preLoad: 1.3,
-    error: '/error.png',  // 錯誤時顯示的圖片路徑
+    error: '/error.jpg',  // 錯誤時顯示的圖片路徑
     loading: '/loading.gif',  // 載入中的圖片
     attempt: 1,  // 嘗試次數
   });
