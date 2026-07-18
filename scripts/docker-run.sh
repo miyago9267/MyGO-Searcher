7c09766dd59764aa4c833f49791c987d723979c8
miyago9267 <miyagomizugen@gmail.com>
2026-07-19 00:19:00 +0800

    fix: 修正 Docker 圖床 runtime 設定

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -3,6 +3,15 @@ user="mygo"
 name="mygo-searcher"
 port="4001"
 
+run_env_args=()
+if [ -f .env.production ]; then
+    image_base_url=$(sed -n 's/^NUXT_IMG_BASE_URL=//p' .env.production | tail -n 1)
+    if [ -n "$image_base_url" ]; then
+        # Nuxt runtimeConfig maps this key to NUXT_NUXT_IMG_BASE_URL.
+        run_env_args+=(--env "NUXT_NUXT_IMG_BASE_URL=$image_base_url")
+    fi
+fi
+
 docker build \
     $@ -t $user/$name:latest . || exit
 [ "$(docker ps | grep $name)" ] && docker kill $name
@@ -14,6 +23,7 @@ docker run \
 	--name $name \
     --network bridge \
     --env-file .env.production \
+    "${run_env_args[@]}" \
     -p $port:3000 \
 	--restart=always \
 	$user/$name:latest
